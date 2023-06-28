b15f70d0a0fe83a49cf0ac56ac69f127a4a92ad1
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-06-28 21:26:47 +0700

    feat: add tree weight to the plot

--- src/ui.py
+++ src/ui.py
@@ -350,6 +350,9 @@ class Ui_MainWindow(object):
             ax.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], color='white')
             ax.text((pos1[0] + pos2[0])/2, (pos1[1] + pos2[1])/2, str(weight), color = 'black', fontweight='bold')
         
+        # add the self.graph.tree_weight to the plot as well
+        ax.text(0.5, 0.05, f"Total Weight: {self.graph.tree_weight()}", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, color='white', fontweight='bold')
+        
         self.canvas.draw()
     
     def findMST(self):
